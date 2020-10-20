a521a644ddf5e6c44c6dfbf869f76c63a371a7fe
Stephen <stephen.germain@free.Fr>
2020-10-20 15:02:17 +0200

    formatting home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(\Swift_Mailer $mailer, Request $request)
     {
-        return $this->render('home/index.html.twig');
+        $formulaireContact = $this->createForm(ContactType::class);
+        $formulaireContact->handleRequest($request);
+
+        if($formulaireContact->isSubmitted() && $formulaireContact->isValid()){
+            $infos = $formulaireContact->getData();
+            $mail = (new \Swift_Message('Demande de contact'))
+                ->setFrom($infos['email'])
+                ->setTo('stephen.germain971@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'contact/email.html.twig', [
+                            'nom' => $infos['nom'],
+                            'mail' => $infos['mail'],
+                            'sujet' => $infos['sujet'],
+                            'message' => $infos['message']
+                        ],
+                        'text/html'
+                    )
+                );
+            $mailer->send($mail);
+            $this->addFlash(
+                'success',
+                'Votre message à bien été envoyé'
+            );
+            return $this->redirectToRoute('home');
+        }
+
+
+        return $this->render('home/index.html.twig',[
+            'formulaireDeContact' =>$formulaireContact->createView(),
+        ]);
     }
 }
